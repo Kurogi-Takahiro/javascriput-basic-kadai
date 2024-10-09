44b0b32d4b3aea75d110817399668786c22eb154
Kurogi-Takahiro <tkurogi@aicom.co.jp>
2024-10-10 02:08:41 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,12 @@
+
+let number = 15
+
+if(number %3 === 0 && number % 5 === 0){
+    console.log("3と5の倍数です")
+}else if(number %3 === 0){
+    console.log("3の倍数です")
+}else if(number %5 === 0){
+    console.log("5の倍数です")
+}else{
+    console.log(number)
+}
