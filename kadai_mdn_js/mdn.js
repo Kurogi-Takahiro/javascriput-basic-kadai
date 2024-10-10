65c7f212fe44ac377fd04634f9c55de8912c2cc4
Kurogi-Takahiro <tkurogi@aicom.co.jp>
2024-10-10 09:17:06 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,6 +2,6 @@ const now = new Date();
 const year = now.getFullYear();
 const month = now.getMonth() + 1;
 const date = now.getDate();
-const YYYYMMDD = `現在の日付は${year}/${month}/${date}です。`;
+const YYYYMMDD = `現在の日付は${year}年${month}月${date}日です。`;
 
 console.log(YYYYMMDD);
